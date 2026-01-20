4c371613437d3c4921282da177c15ad0f5849577
Ben Clive <ben.clive@grafana.com>
2026-01-20 14:14:46 +0000

    Propagate ambiguous columns through distributed metastore calls

--- pkg/engine/internal/planner/planner_test.go
+++ pkg/engine/internal/planner/planner_test.go
@@ -93,7 +93,7 @@ var mockedMetastoreSections = []*metastore.DataobjSectionDescriptor{
 		Size:      1 << 10,
 		Start:     time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC),
 		End:       time.Date(2025, time.January, 1, 0, 30, 0, 0, time.UTC),
-		LabelsByStreamID: map[int64][]string{
+		AmbiguousPredicatesByStream: map[int64][]string{
 			1: {"app", "one", "foo", "bar"},
 			2: {"app", "two", "foo", "bar"},
 		},
@@ -108,7 +108,7 @@ var mockedMetastoreSections = []*metastore.DataobjSectionDescriptor{
 		Size:      1 << 10,
 		Start:     time.Date(2025, time.January, 1, 0, 30, 0, 0, time.UTC),
 		End:       time.Date(2025, time.January, 1, 1, 0, 0, 0, time.UTC),
-		LabelsByStreamID: map[int64][]string{
+		AmbiguousPredicatesByStream: map[int64][]string{
 			1: {"app", "one", "foo", "bar"},
 			2: {"app", "two", "foo", "bar"},
 		},
